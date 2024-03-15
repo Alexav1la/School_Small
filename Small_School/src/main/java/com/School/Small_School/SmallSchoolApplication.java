e9d32dfe34a13fa82ff759bb15941109e770016d
Alex  Avila <144721172+Alexav1la@users.noreply.github.com>
2024-03-14 19:03:32 -0500

    primer commit

---
+++ Small_School/src/main/java/com/School/Small_School/SmallSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.School.Small_School;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallSchoolApplication.class, args);
+	}
+
+}
---
+++ Small_School/src/test/java/com/School/Small_School/SmallSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.School.Small_School;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
